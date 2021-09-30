a30b0b27d4e98a85da27739a408bab8dcb41ea19
volha bukhal <o.bukhal@gmail.com>
2021-09-30 15:19:55 +0300

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,18 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let repeatTimes = options.repeatTimes || null;
+  let separator = options.separator || '+';
+  let addition = options.addition || null;
+  let additionRepeatTimes = options.additionRepeatTimes || null;
+  let additionSeparator = addition ? (options.additionSeparator || '|') : null;
+
+  let bigAddition = '';
+  if (addition) {
+    bigAddition = Array(additionRepeatTimes).fill(addition).join(additionSeparator)
+  }
+  let strChunk = str + bigAddition;
+  let result = Array(repeatTimes).fill(strChunk).join(separator)
+  return result
 }
